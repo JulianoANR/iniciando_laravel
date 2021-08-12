d3925b46c589dd0fc33e4890538417b251924beb
JulianoANR <86992122+JulianoANR@users.noreply.github.com>
2021-08-12 12:22:01 -0300

    Aula 10
    Da aula 7 a 10. Blade, funçoes.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function __invoke(){
-        return "Vc entrou na home";
+        return view('home');
     }
 }
--- app/Http/Controllers/ProdutosController.php
+++ app/Http/Controllers/ProdutosController.php
@@ -6,19 +6,22 @@ use Illuminate\Http\Request;
 
 class ProdutosController extends Controller{  
     public function index(){
-        return "pagina de produtos";
+        return view('produtos.index');
     }
 
     public function create(){
-        return "pagina de inserir produtos";
+        return view('produtos.create');
     }
 
     public function show($nome, $valor){//função coma parametros opicionais
+        return view('produtos.show', ['nome' => $nome,'valor' => $valor]);
+        /*
         if($valor){
             return "o Nome do produto é $nome, e seu valor é $valor";
         }else{
             return "o Nome do produto é $nome";
         }
+        */
     }
 
 }
