2e2d0ac4912c3d5419304270e1fa56fabc4c32c4
JulianoANR <86992122+JulianoANR@users.noreply.github.com>
2021-08-13 15:18:21 -0300

    Aula 15

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,9 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     <title>@yield('title')</title>
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="./">Home</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="produtos">Produtos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="produtos/inserir">Clientes</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="usuarios">Usuarios</a>
+        </li>
+       
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
 @yield('content')
+
+</script>
 </body>
 </html>
--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -2,10 +2,12 @@
 @section('title', 'Pagina index')
 @section('content')
 
-<h1>Pagina dos Produtos</h1>
+<div class="containt"></div>
+<a href="produtos/inserir" type="button" class="mt-4 mb-4 btn btn-secondary">Inserir</a>
 @foreach ($produtos as $produto)
 <p>{{$produto->nome}}</p>
 @endforeach
 {{$produtos->links()}}
+</div>
 
 @endsection
