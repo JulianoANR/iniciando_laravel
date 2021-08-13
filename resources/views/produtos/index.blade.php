e6c513d6821d22c3e582f843740672637433797e
JulianoANR <86992122+JulianoANR@users.noreply.github.com>
2021-08-13 15:55:06 -0300

    cont

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -2,12 +2,41 @@
 @section('title', 'Pagina index')
 @section('content')
 
-<div class="containt"></div>
-<a href="produtos/inserir" type="button" class="mt-4 mb-4 btn btn-secondary">Inserir</a>
-@foreach ($produtos as $produto)
-<p>{{$produto->nome}}</p>
-@endforeach
-{{$produtos->links()}}
+<div class="container m-4"></div>
+<a href="produtos/inserir" type="button" class="ml-4 mt-4 mb-4 btn btn-secondary">Inserir Produto</a>
+
+<!-- DataTales Example -->
+<div class="card shadow mb-4">
+
+<div class="card-body">
+  <div class="table-responsive">
+    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Valor</th>
+          <th>Estoque</th>
+          <th>Ações</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        @foreach ($produtos as $produto)
+        <tr>
+            <td>{{$produto->nome}}</td>
+            <td>{{$produto->valor}}</td>
+            <td>{{$produto->estoque}}</td>
+            <td>Ações</td>
+        </tr>
+        @endforeach
+      </tbody>
+  </table>
+</div>
+</div>
+</div>
+
+
+<!-- {{$produtos->links()}}-->
 </div>
 
 @endsection
