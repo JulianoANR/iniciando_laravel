13b24cba3708b295a7ec97af6fd5c0ef9ba67d87
JulianoANR <juliano.appe@gmail.com>
2021-08-13 23:29:16 -0300

    Datatable complete
    Finalizando

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -2,43 +2,58 @@
 @section('title', 'Pagina index')
 @section('content')
 
-<div class="container m-4"></div>
-<a href="{{route('produtos.inserir')}}" type="button" class="ml-4 mt-4 mb-4 btn btn-secondary">Inserir Produto</a>
+@section('extra-styles')
+<style>
+  i {
+    color: red;
+  }
+</style>  
+@endsection
 
-<!-- DataTales Example -->
-<div class="card shadow mb-4">
+<div class="container-fluid p-3">
+    <a href="{{route('produtos.inserir')}}" type="button" class="ml-4 mt-4 mb-4 btn btn-secondary">Inserir Produto</a>
 
-<div class="card-body">
-  <div class="table-responsive">
-    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-      <thead>
-        <tr>
-          <th>Nome</th>
-          <th>Valor</th>
-          <th>Estoque</th>
-          <th>Ações</th>
-        </tr>
-      </thead>
+    <!-- DataTables Example -->
+    <div class="card shadow mb-4">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered table-sm table-striped table-hover" id="dataTable" width="100%"
+                    cellspacing="0">
+                    <thead>
+                        <tr>
+                          <th>Nome</th>
+                          <th>Valor</th>
+                          <th>Estoque</th>
+                          <th>Ações</th>
+                        </tr>
+                    </thead>
 
-      <tbody>
-        @foreach ($produtos as $produto)
-        <tr>
-            <td>{{$produto->nome}}</td>
-            <td>{{$produto->valor}}</td>
-            <td>{{$produto->estoque}}</td>
-            <td>
-             <a href="{{route('produtos.descricao', "$produto->id")}}"><i class="fas fa-eye text-primary"></i></a>        
-            </td>
-        </tr>
-        @endforeach
-      </tbody>
-  </table>
+                    <tbody>
+                        @foreach ($produtos as $produto)
+                        <tr>
+                            <td>{{$produto->nome}}</td>
+                            <td>{{$produto->valor}}</td>
+                            <td>{{$produto->estoque}}</td>
+                            <td>
+                                <a href="{{route('produtos.descricao', "$produto->id")}}"><i
+                                        class="fas fa-eye text-danger fa-lg"></i></a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
+
+{{-- {{$produtos->links()}} --}}
 </div>
 
-
-{{$produtos->links()}}
-</div>
+{{-- <script>
+    $(document).ready(function () {
+        alert("Teste")
+    });
+</script> --}}
 
 @endsection
